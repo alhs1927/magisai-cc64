3a6d9f0fa87bf7ce3b45b87058d5b2c6e5c78623
alhs1927 <139057395+alhs1927@users.noreply.github.com>
2026-01-17 18:24:14 +0700

    streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,7 +11,7 @@ import edge_tts
 # --- 1. KONFIGURASI HALAMAN ---
 st.set_page_config(
     page_title="Magis AI - Jesuit Order Edition",
-    page_icon="🕊️",
+    page_icon="logo magis ai.png",
     layout="wide",
     initial_sidebar_state="expanded",
 )
@@ -631,4 +631,5 @@ if st.session_state.result_text:
             file_name=f"MagisAI_{L_CODE}.docx",
             mime="application/vnd.openxmlformats-officedocument.wordprocessingml.document",
             use_container_width=True
+
         )
